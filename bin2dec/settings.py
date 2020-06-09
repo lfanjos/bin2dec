66ade82f46a0089d6f8a54328cf0b043cfa09487
Lucas <lfanjosus@gmail.com>
2020-06-09 15:49:11 -0300

    Adição do gitignore e configurações de middleware

--- bin2dec/settings.py
+++ bin2dec/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^vy@igwr8b7eggyer^bt2*_puk-f(!b)od#zei#dmz2z-ikj)q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
